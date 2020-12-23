023a844380bc49347fb6ddcdc1b36502f032d6ac
kislykamil <kislykamil@gmail.com>
2020-12-23 22:48:22 +0100

    Removed unused imports from tests

--- src/test/java/WebApp/EasyLearn/IntegrationTest.java
+++ src/test/java/WebApp/EasyLearn/IntegrationTest.java
@@ -1,21 +1,8 @@
 package WebApp.EasyLearn;
 
 import WebApp.EasyLearn.controller.DetailsController;
-import WebApp.EasyLearn.controller.ExamController;
-import WebApp.EasyLearn.model.ExamStats;
-import WebApp.EasyLearn.model.User;
-import WebApp.EasyLearn.model.request.ExamSubmitRequest;
-import WebApp.EasyLearn.model.response.DetailsResponse;
-import WebApp.EasyLearn.repository.UserDetailRepository;
-import WebApp.EasyLearn.service.DetailService;
-import WebApp.EasyLearn.service.ExamStatsService;
-import WebApp.EasyLearn.util.JwtUtil;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
